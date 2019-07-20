251a86f90cb4759b15ac7cf0c8683e1c54306643
Caitlin Prentke <caitlin.prentke@gmail.com>
2019-07-20 15:18:59 -0400

    npm install beautiful dnd, and add image clear

--- src/components/imageDisplay.jsx
+++ src/components/imageDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react'
 
 const ImageDisplay = () => {
-  const [ image, updateImage ] = useState("");
+  const [ image, updateImage ] = useState("https://images.squarespace-cdn.com/content/v1/531d09e9e4b025f65847ba91/1548005908980-2P5870ROH4E18WDL1I87/ke17ZwdGBToddI8pDm48kDqFA6F2bdj-odyieWaIIzV7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Ue4ISNS0LB8b7S_RzFTzjXasR14DQkyTwibcWsfKbwqmujUcYgnCrl657jva-OwOHQ/L4177R2-R01-014.Jpg?format=1000w");
 
   const useDisplayNewImage = () => {
     const reader = new FileReader();
@@ -9,15 +9,24 @@ const ImageDisplay = () => {
     reader.onload = () => {
       updateImage(reader.result);
     }
+    if (file) {
     reader.readAsDataURL(file);
+  }
   };
+
+  let clearButton;
+
+  if (image.length > 0) {
+    clearButton = <button onClick={() => updateImage("")}>Clear Image</button>
+  }
 
   return(
     <Fragment>
       <div>Image Display</div>
-      <input type="file" accept="jpg" id="file" />
+      <input type="file" accept="jpg gif" id='file' />
       <button onClick={() => useDisplayNewImage()}>Upload Image</button>
-      <img src={image} />
+      {clearButton}
+      <img id="image" src={image} />
     </Fragment>
   )
 };
