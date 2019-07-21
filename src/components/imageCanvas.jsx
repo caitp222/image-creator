6dc46bb442bec9071eb09371dc7c4296fa2b4e42
Caitlin Prentke <caitlin.prentke@gmail.com>
2019-07-20 20:25:16 -0400

    turn upload, delete and save into icons

--- src/components/imageCanvas.jsx
+++ src/components/imageCanvas.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 
 const ImageCanvas = (props) => {
-  const { imageSrc } = props;
+  const {
+    imageSrc,
+    downloadButton,
+    clearButton,
+    useDisplayNewImage,
+    displayFrame,
+    useToggleDisplayFrame
+  } = props;
+
+
   return(
     <div>
-      <img id="image" src={imageSrc} />
+      <div className="row">
+        <div className="col s12 m7">
+          <div className="card">
+            <div className="card-image">
+              <img
+                id="image"
+                className="stacked-images"
+                src={imageSrc}
+              />
+              {
+                displayFrame ?
+                  <img
+                    id="frame"
+                    className="stacked-images"
+                    src={'https://www.pngfind.com/pngs/m/80-804703_ornate-picture-frame-png-arc-studios-gallery-transparent.png'}
+                  /> :
+                  null
+              }
+              <span className="card-title">
+                <div className="file-field input-field">
+                  <i className="material-icons">add_a_photo</i>
+                  <input onChange={useDisplayNewImage} type="file" id="file" accept="jpg gif" />
+                </div>
+              </span>
+            </div>
+            <div className="card-action">
+              <a href="#" onClick={useToggleDisplayFrame}>{displayFrame ? "Remove Frame" : "Add Frame"}</a>
+            </div>
+            <div className="card-content">
+              {clearButton}
+              {downloadButton}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
--- src/components/imageDisplay.jsx
+++ src/components/imageDisplay.jsx
@@ -1,9 +1,10 @@
 import React, { Fragment, useState } from 'react'
-import Button from './miniComponents/Button';
 import ImageCanvas from './imageCanvas';
+import ImageDisplayHeader from './imageDisplayHeader';
 
 const ImageDisplay = () => {
   const [ image, updateImage ] = useState("https://images.squarespace-cdn.com/content/v1/531d09e9e4b025f65847ba91/1548005908980-2P5870ROH4E18WDL1I87/ke17ZwdGBToddI8pDm48kDqFA6F2bdj-odyieWaIIzV7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Ue4ISNS0LB8b7S_RzFTzjXasR14DQkyTwibcWsfKbwqmujUcYgnCrl657jva-OwOHQ/L4177R2-R01-014.Jpg?format=1000w");
+  const [ displayFrame, updateDisplayFrame ] = useState(true);
 
   const useDisplayNewImage = () => {
     const reader = new FileReader();
@@ -16,30 +17,35 @@ const ImageDisplay = () => {
     }
   };
 
+  const useToggleDisplayFrame = () => {
+    updateDisplayFrame(!displayFrame);
+  }
+
   let clearButton, downloadButton;
 
   if (image.length > 0) {
     const clearImage = () => updateImage("");
-    clearButton = <Button
-      onClick={clearImage}
-      innerText={"Clear Image"}
-    />
-
-    downloadButton = <a href={image} download={"image.jpg"}>Download Image</a>;
-
+    clearButton = <i onClick={clearImage} className="material-icons">delete_outline</i>
+    downloadButton = <a href={image} download={"image.jpg"} className="material-icons">save_alt</a>;
   }
 
   return(
     <Fragment>
-      <div>Image Display</div>
-      <input type="file" accept="jpg gif" id='file' />
-      <Button
-        onClick={useDisplayNewImage}
-        innerText={"Upload Image"}
+      <ImageDisplayHeader />
+      <div id="icon-row">
+        <div className="file-field input-field">
+          <i className="material-icons">add_a_photo</i>
+          <input onChange={useDisplayNewImage} type="file" id="file" accept="jpg gif" />
+        </div>
+      </div>
+      <ImageCanvas
+        imageSrc={image}
+        clearButton={clearButton}
+        downloadButton={downloadButton}
+        useDisplayNewImage={useDisplayNewImage}
+        displayFrame={displayFrame}
+        useToggleDisplayFrame={useToggleDisplayFrame}
       />
-      {clearButton}
-      {downloadButton}
-      <ImageCanvas imageSrc={image} />
     </Fragment>
   )
 };
